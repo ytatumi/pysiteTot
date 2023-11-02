7fff494ba7b3a8409ef5f15537b0b57bb2593cf9
Jonas Jons <jonas.jons@consid.se>
2023-11-02 09:56:01 +0100

    Updated for Project assignment

--- PySite.API/flask_app/routes.py
+++ PySite.API/flask_app/routes.py
@@ -1,22 +1,43 @@
 from flask_app import app
 from flask import jsonify, request
 
-# Initialize the users list with a single user
-users = [{"name": "Initial User"}]
+# Add initial User
+current_id = 0
+users = {str(current_id): {"name": "Jonass"}}
 
 
 @app.route("/api/users", methods=["GET", "POST"])
 def handle_users():
+    global current_id
     if request.method == "GET":
         return jsonify({"users": users})
+
     elif request.method == "POST":
-        data = request.get_json()
-        # Example: data should contain user information to add or update
-        for user in data:
-            users.append(user)
-        return jsonify({"message": "Users updated successfully"})
+        # Recieved new request to add a user
+        try:
+            data = request.get_json()
+            print("printing the data recieved from frontend", data)
+            current_id += 1
+            users[str(current_id)] = data
+
+            return jsonify({"message": "Users updated successfully"})
+        except Exception as e:
+            print("Error occured: ", e)
+            return jsonify({"message": "could not update users"}), 500
 
 
-@app.route("/")
-def hometest():
-    return {"user": "kenny", "message": "Message from home"}
+@app.route("/api/users/<user_id>", methods=["GET", "PUT", "DELETE"])
+def handle_user(user_id):
+    """
+    This function takes an ID as dynamic URL parameter, for example 0
+    Note:
+    - GET method is only available for debugging purposes, to test for a specific user
+    open the browser with for example http://localhost:5000/api/users/0 to see the data
+
+    - The assignment is to implement the PUT and DELETE methods for the specific ID, see assignment for specific instructions
+    """
+    if user_id in users:
+        if request.method == "GET":
+            return jsonify(users[user_id])
+    else:
+        return jsonify({"message": "User not found"}), 404
